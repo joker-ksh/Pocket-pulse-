54051dfa1192da547819e4d01614ef6b83680828
yashwanth-sri-sai <kuchampudi22bcy25@iiitkottayam.ac.in>
2024-03-13 12:09:21 +0530

    change

--- register.js
+++ register.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 // third change
 //yash github first time 
+// change added
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import {
   getAuth,
