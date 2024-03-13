3948cfe76a23447bfaebd2158893ae083bf3fb74
Enrique005D <rahulenrique05@gmail.com>
2024-03-13 11:14:37 +0530

    rahul aya

--- register.js
+++ register.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 // third change
+
+//rahul is editong the code
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import {
   getAuth,
